4768b17986d98506fb391408cca771eeeec80f23
Sthuthi98 <sthuthisadananda@gmail.com>
2023-09-21 15:05:34 +0200

    testing

--- agent_code/my_agent_v2/callbacks.py
+++ agent_code/my_agent_v2/callbacks.py
@@ -4,7 +4,7 @@ import random
 import numpy as np
 import settings as s
 
-ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT']
+ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT','BOMB']
 
 def setup(self):
     """
@@ -31,17 +31,38 @@ def setup(self):
         print(self.epsilon)
 
 
-def epsilon_greedy_policy(self,state):
+def softmax_action_selection(Q_values, temperature=1.0):
+    Q_values[np.isneginf(Q_values)] = -1e9
+    Q_values=Q_values/1000
+    probabilities = np.exp(Q_values / temperature)
+    action_probabilities = probabilities / np.sum(probabilities)
+    #print(action_probabilities)
+    chosen_action = np.random.choice(ACTIONS, p=action_probabilities)
+    return chosen_action
+
+
+def epsilon_greedy_policy_bkp(self,state):
     if np.random.rand() < self.epsilon:
         self.logger.info("Random action.")
         return np.random.choice(ACTIONS,p=[.25, .25, .25, .25])
     else:
         self.logger.info("Model based action.")
         q_values=self.Qtable[state[0],state[1], :]
+        
         if all(q == q_values[0] for q in q_values):
             return np.random.choice(ACTIONS,p=[.25, .25, .25, .25])
         else:
             return ACTIONS[np.argmax(self.Qtable[state[0],state[1], :])]
+        
+def epsilon_greedy_policy(self,state):
+    if np.random.rand() < self.epsilon:
+        self.logger.info("Random action.")
+        return np.random.choice(ACTIONS,p=[.24, .24, .24, .24,0.04])
+    else:
+        self.logger.info("Model based action.")
+        q_values=self.Qtable[state[0],state[1], :]
+        #print(q_values)
+        return softmax_action_selection(q_values, temperature=1.0)
 
 def act(self, game_state: dict) -> str:
     """
@@ -52,25 +73,33 @@ def act(self, game_state: dict) -> str:
     :param game_state: The dictionary that describes everything on the board.
     :return: The action to take as a string.
     """
+
     state = state_to_features(game_state)
     action = epsilon_greedy_policy(self,state)
-    self.logger.info(state)
-    self.logger.info(action)
+    self.logger.debug(state)
+    self.logger.debug(action)
     return action
 
 def situational_awareness(game_state):
     x,y=game_state['self'][3]
     return {'UP':game_state['field'][x][y+1],'RIGHT':game_state['field'][x+1][y],'DOWN':game_state['field'][x][y-1],'LEFT':game_state['field'][x-1][y]}
 
+
+
+"""
 def coins_in_row(game_state):
     coins=game_state['coins']
     agent_x, agent_y = game_state['self'][3]
+
+    #Check for coins in each directions
     left_coins = any(coin_x < agent_x and coin_y == agent_y for coin_x, coin_y in coins)
     right_coins = any(coin_x > agent_x and coin_y == agent_y for coin_x, coin_y in coins)
     up_coins= any(coin_y < agent_y and coin_x == agent_x for coin_x, coin_y in coins)
     down_coins= any(coin_y < agent_y and coin_x == agent_x for coin_x, coin_y in coins)
 
     return {'UP':up_coins,'RIGHT': right_coins,'DOWN':down_coins,'LEFT':left_coins}
+"""
+
 
 def state_to_features(game_state: dict) -> np.array:
     """
@@ -87,6 +116,7 @@ def state_to_features(game_state: dict) -> np.array:
     :return: np.array
     """
     # This is the dict before the game begins and after it ends
+    #print(game_state['explosion_map'])
     features=[]
     if game_state is None:
         return None
@@ -96,7 +126,11 @@ def state_to_features(game_state: dict) -> np.array:
     features.append(agent_x) 
     features.append(agent_y)
     features.append(situational_awareness(game_state))
-    features.append(coins_in_row(game_state))
+    #features.append(coins_in_row(game_state))
+    #features.append(find_direction_with_max_coins(game_state))
+    #features.append(bomb_trajectory(game_state,escape_path))
+    #features.append(find_direction_with_max_bombs(game_state))
+
     #print(features)
     return features
 
--- agent_code/my_agent_v2/train.py
+++ agent_code/my_agent_v2/train.py
@@ -7,7 +7,7 @@ import settings as s
 from .callbacks import state_to_features
 import os
 
-ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT']
+ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT','BOMB']
 
 alpha = 0.1 # Learning rate
 gamma = 0.9  # Discount factor
@@ -15,7 +15,7 @@ initial_epsilon = 0.9  # Initial epsilon value
 
 min_initial_value = 0.01
 max_initial_value = 0.1
-min_epsilon = 0.1     # Minimum epsilon value
+min_epsilon = 0.5     # Minimum epsilon value
 epsilon_decay_rate = 0.1  # Amount by which epsilon is reduced after each episode
 
 
@@ -32,6 +32,9 @@ REVISIT_STATE="REVISIT_STATE"
 COINS_HAUL_20="COINS_HAUL_20"
 COINS_HAUL_50="COINS_HAUL_50"
 LOW_COINS_HAUL="LOW_COINS_HAUL"
+AVG_COINS_HAUL="AVG_COINS_HAUL"
+MOVED_AWAY_FROM_BOMB="MOVED_AWAY_FROM_BOMB"
+
 
 def setup_training(self):
     """
@@ -44,16 +47,23 @@ def setup_training(self):
     # Example: Setup an array that will note transition tuples
     # (s, a, r, s')
     #self.transitions = deque(maxlen=TRANSITION_HISTORY_SIZE)
-    width = s.COLS-1
-    height = s.ROWS-1
-    #num_states = width * height 
+    width = s.COLS
+    height = s.ROWS
+    #num_states = width * height
+    #initial_value=0
     num_actions=len(ACTIONS)
     if not os.path.isfile("my-saved-model.pt"):
         #self.Qtable=np.random.uniform(min_initial_value, max_initial_value, size=(width,height, num_actions))
         self.Qtable=np.zeros((width,height, num_actions))
+        #self.Qtable=np.full((width,height, num_actions),initial_value)
     else:
+        print("Loading existing Qtable")
         with open("my-saved-model.pt", "rb") as file:
             self.Qtable = pickle.load(file)
+        #extra_action_qvalues = np.zeros((width, height, 1))
+        #new_Qtable = np.concatenate((self.Qtable, extra_action_qvalues), axis=2)
+        #self.Qtable= new_Qtable
+    print(self.Qtable.shape)
     self.reward=0
     self.epsilon=initial_epsilon
     self.visited_states=[]
@@ -63,9 +73,12 @@ def setup_training(self):
 
 def q_learning_update(self,state, action, reward, next_state):
     current_q = self.Qtable[state[0],state[1], action]
-    max_next_q = np.max(self.Qtable[next_state[0],next_state[1], :])
-    new_q = current_q + alpha * (reward + gamma * max_next_q - current_q)
-    self.Qtable[state[0],state[1], action] = new_q
+    if current_q==-np.inf:
+        pass
+    else:
+        max_next_q = np.max(self.Qtable[next_state[0],next_state[1], :])
+        new_q = current_q + alpha * (reward + gamma * max_next_q - current_q)
+        self.Qtable[state[0],state[1], action] = new_q
 
 def game_events_occurred(self, old_game_state: dict, self_action: str, new_game_state: dict, events: List[str]):
     """
@@ -84,19 +97,27 @@ def game_events_occurred(self, old_game_state: dict, self_action: str, new_game_
     :param new_game_state: The state the agent is in now.
     :param events: The events that occurred when going from  `old_game_state` to `new_game_state`
     """
-    
-    if new_game_state['step']==1:
-        print(" New Round",new_game_state['round'],"\nReward",self.reward)
+    if old_game_state['step']==1 and old_game_state['round']==1:
+        x,y = np.where(old_game_state['field']== -1)
+        self.Qtable[x,y, :]= -np.inf
+    if old_game_state['step']==1:
+        print(" New Round",new_game_state['round'],"\nReward",self.reward,"\nPosition",old_game_state['self'][3])
     old_score=old_game_state['self'][1]
     new_score=new_game_state['self'][1]
     if new_score>old_score:
         events.append(SCORE_INCREASE)
     if new_game_state['step']>1:
-        if state_to_features(new_game_state)[:2]==self.visited_states[-1]:
+        if state_to_features(new_game_state)[:2]==self.visited_states[-1] and e.BOMB_DROPPED in events:
             events.append(REVISIT_STATE)
     self.visited_states.append(state_to_features(old_game_state)[:2])
     self.reward+=reward_from_events(self,events,old_game_state)
+    #with open("run_log.txt", "a") as file:
+        #file.write("--------------------------------------\n")
+        #file.write(str(self.Qtable[state_to_features(old_game_state)[0],state_to_features(old_game_state)[1]]))
+        #file.write(str(self_action)+str(ACTIONS.index(self_action))+str(self.reward))
     q_learning_update(self,state_to_features(old_game_state), ACTIONS.index(self_action), self.reward,state_to_features(new_game_state))
+    #with open("run_log.txt", "a") as file:
+        #file.write(str(self.Qtable[state_to_features(old_game_state)[0],state_to_features(old_game_state)[1]]))
     self.logger.debug(f'Encountered game event(s) {", ".join(map(repr, events))} in step {new_game_state["step"]}')
 
     # state_to_features is defined in callbacks.py
@@ -134,6 +155,97 @@ def end_of_round(self, last_game_state: dict, last_action: str, events: List[str
     with open("my-saved-model.pt", "wb") as file:
         pickle.dump(self.Qtable, file)
 
+def find_direction_with_max_coins(game_state):
+    coins = game_state['coins']
+    agent_x, agent_y = game_state['self'][3]
+
+    # Initialize counters for each direction
+    left_coins_count = 0
+    right_coins_count = 0
+    up_coins_count = 0
+    down_coins_count = 0
+
+    # Iterate through coins and count coins in each direction
+    for coin_x, coin_y in coins:
+        if coin_x < agent_x and coin_y == agent_y:
+            left_coins_count += 1
+        elif coin_x > agent_x and coin_y == agent_y:
+            right_coins_count += 1
+        elif coin_y < agent_y and coin_x == agent_x:
+            up_coins_count += 1
+        elif coin_y > agent_y and coin_x == agent_x:
+            down_coins_count += 1
+
+    # Create a dictionary to store counts for each direction
+    coin_counts = {
+        'LEFT': left_coins_count,
+        'RIGHT': right_coins_count,
+        'UP': up_coins_count,
+        'DOWN': down_coins_count
+    }
+
+    # Find the direction with the maximum number of coins
+    max_direction = max(coin_counts, key=coin_counts.get)
+
+    return max_direction
+
+def find_direction_with_max_bombs(game_state):
+    bombs = game_state['bombs']
+    agent_x, agent_y = game_state['self'][3]
+
+    # Initialize counters for each direction
+    left_bombs_count = 0
+    right_bombs_count = 0
+    up_bombs_count = 0
+    down_bombs_count = 0
+
+    # Iterate through bombs and count bombs in each direction
+    for (bomb_x, bomb_y), countdown in bombs:
+        if bomb_x < agent_x and bomb_y == agent_y:
+            left_bombs_count += 1
+        elif bomb_x > agent_x and bomb_y == agent_y:
+            right_bombs_count += 1
+        elif bomb_y < agent_y and bomb_x == agent_x:
+            up_bombs_count += 1
+        elif bomb_y > agent_y and bomb_x == agent_x:
+            down_bombs_count += 1
+
+    # Create a dictionary to store counts for each direction
+    bomb_counts = {
+        'LEFT': left_bombs_count,
+        'RIGHT': right_bombs_count,
+        'UP': up_bombs_count,
+        'DOWN': down_bombs_count
+    }
+
+    # Find the direction with the maximum number of bombs
+    max_direction = max(bomb_counts, key=bomb_counts.get)
+
+    return max_direction
+
+def avoid_bomb_trajectory(game_state):
+    action_ideas=[]
+    x,y=game_state['self'][3]
+    for (xb, yb), t in game_state['bombs']:
+        if (xb == x) and (abs(yb - y) < 4):
+            if (yb > y): action_ideas.append('UP')
+            if (yb < y): action_ideas.append('DOWN')
+            action_ideas.append('LEFT')
+            action_ideas.append('RIGHT')
+        if (yb == y) and (abs(xb - x) < 4):
+            if (xb > x): action_ideas.append('LEFT')
+            if (xb < x): action_ideas.append('RIGHT')
+            action_ideas.append('UP')
+            action_ideas.append('DOWN')
+    return action_ideas
+
+def are_valid(x,y,game_state):
+    if game_state['field'][x][y]==0:
+        return True
+    else:
+        return False
+    
+
 
 def reward_from_events(self, events: List[str],game_state) -> int:
     """
@@ -143,39 +255,75 @@ def reward_from_events(self, events: List[str],game_state) -> int:
     certain behavior.
     """
     features=state_to_features(game_state)[2]
-    coins=state_to_features(game_state)[3]
+    x,y=game_state['self'][3]
+    #coins=state_to_features(game_state)[3]
+    max_coins=find_direction_with_max_coins(game_state)
+    #max_bombs=find_direction_with_max_bombs(game_state)
+    escape_route={
+        'UP&LEFT':(x-1,y+1),
+        'UP&RIGHT':(x+1,y+1),
+        'DOWN&LEFT':(x-1,y-1),
+        'DOWN&RIGHT':(x+1,y-1)
+    }
     action_event_mapping={
-        'UP':e.MOVED_UP, 'DOWN':e.MOVED_DOWN,'LEFT':e.MOVED_LEFT, 'RIGHT':e.MOVED_RIGHT
+        'UP':e.MOVED_UP, 'DOWN':e.MOVED_DOWN,'LEFT':e.MOVED_LEFT, 'RIGHT':e.MOVED_RIGHT, 'BOMB':e.BOMB_DROPPED
     }
     game_rewards = {
-        e.INVALID_ACTION:-2,
-        e.MOVED_LEFT:1,
-        e.MOVED_RIGHT:1,
-        e.MOVED_UP:1,
-        e.MOVED_DOWN:1 ,
-        e.COIN_COLLECTED:8,
+        e.INVALID_ACTION:-1,
+        e.MOVED_LEFT:0,
+        e.MOVED_RIGHT:0,
+        e.MOVED_UP:0,
+        e.MOVED_DOWN:0,
+        e.BOMB_DROPPED:0,
+        e.COIN_FOUND:8,
+        e.CRATE_DESTROYED:4,
+        e.KILLED_SELF:-15,
+        e.GOT_KILLED:-15,
+        e.BOMB_EXPLODED:1,
+        e.COIN_COLLECTED:4,
         SCORE_INCREASE:4,
-        REVISIT_STATE:-2,
-        COINS_HAUL_20:20,
-        COINS_HAUL_50:100,
-        LOW_COINS_HAUL:-20,
+        REVISIT_STATE:2, 
+        COINS_HAUL_20:10,
+        COINS_HAUL_50:15,
+        LOW_COINS_HAUL:-2.5,
+        AVG_COINS_HAUL:2.5,
+        e.SURVIVED_ROUND:25,
+        MOVED_AWAY_FROM_BOMB:10
     }
     for action, value in features.items():
         if value == -1:
-            game_rewards[action_event_mapping[action]] += -1
+            game_rewards[action_event_mapping[action]] =0
         elif value == 0:
             game_rewards[action_event_mapping[action]] = 1
-            if coins[action] is True:
+            if action==max_coins and not e.BOMB_DROPPED:
                 game_rewards[action_event_mapping[action]] += 1
-    if e.SURVIVED_ROUND in events:
+        elif value==1:
+            for actions,(x,y) in escape_route.items():
+                if are_valid(x,y,game_state):
+                    for a in actions.split('&'):
+                        game_rewards[action_event_mapping[action]] += 2
+                        if game_state['self'][2]:
+                            game_rewards[action_event_mapping['BOMB']]=2
+    if e.BOMB_DROPPED and not e.BOMB_EXPLODED:
+        for actions,(x,y) in escape_route.items():
+                if are_valid(x,y,game_state):
+                    for a in actions.split('&'):
+                        game_rewards[action_event_mapping[action]] += 2
+    if  e.BOMB_EXPLODED and not e.KILLED_SELF:
+        events.append(MOVED_AWAY_FROM_BOMB)
+    """if e.SURVIVED_ROUND in events:
         score=game_state['self'][1]
         if score>=20:
             events.append(COINS_HAUL_20)
         elif score==50:
             events.append(COINS_HAUL_50)
-        elif score<20:
+        elif score<10:
             events.append(LOW_COINS_HAUL)
-    #print(game_rewards)
+        else:
+            events.append(AVG_COINS_HAUL)
+    """
+    
+    print(game_rewards)
     reward_sum = 0
     for event in events:
         if event in game_rewards:
